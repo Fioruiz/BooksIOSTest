60e8b7088a697967255ba8d24376500570c623ec
Fiorela <fruiz4827@gmail.com>
2019-06-21 16:52:47 -0500

    Proyecto de libreria ios

---
+++ Libreria/LibrosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LibrosTableViewCell.swift
+//  Libreria
+//
+//  Created by Fiorela Ruiz Castillo on 24/08/18.
+//  Copyright © 2018 Fiorela Ruiz Castillo. All rights reserved.
+//
+
+import UIKit
+
+class LibrosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
